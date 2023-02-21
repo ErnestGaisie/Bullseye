1362476786de60f37a9784949186945ae0f1bd74
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2023-02-21 12:23:58 +0000

    SwiftUI Views

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Xcode!")
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+            }
+            Button("Hit me") {
+                
+            }
         }
-        .padding()
     }
 }
 
