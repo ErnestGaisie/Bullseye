36a783c2855bfef6f0d3f0a39b1c584038bd9745
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2023-02-22 17:47:24 +0000

    Create model

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Gaisie on 2023/2/22.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 37
+    var score: Int = 0
+    var rount: Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+}
