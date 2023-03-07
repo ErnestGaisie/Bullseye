d181b5a81629af744989d56fa51a2f99088da438
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2023-03-07 15:05:05 +0000

    Type inference

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -8,18 +8,14 @@
 import Foundation
 
 struct Game {
-    var target: Int = Int.random(in: 1...100)
-    var score: Int = 0
-    var rount: Int = 1
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var rount = 1
     
     func points(sliderValue: Int) -> Int {
-        var difference: Int = target - sliderValue
-        if difference < 0 {
-            difference = difference * -1
-            //or difference *= -1
-        }
+        let difference = abs(target - sliderValue)
         
-        var awardedPoints: Int = 100 -  difference
+        let awardedPoints = 100 -  difference
         return awardedPoints
     }
 }
